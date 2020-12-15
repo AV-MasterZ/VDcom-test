b80e7088f9d796668f621ddbdd0dc7b5c5150a83
Александр Жадаев <zhadaev_av@srvhub.ru>
2020-12-15 15:12:02 +0400

    Решена задача 1.3

---
+++ converter/src/main/java/org/zhadaev/vdcomtest/converter/Converter.java
@@ -0,0 +1,54 @@
+package org.zhadaev.vdcomtest.converter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Converter {
+
+    public static void main(String... args) {
+        String filePath = getCurrentPath();
+        if (args.length == 0) {
+            filePath = filePath.concat("in.txt");
+        } else {
+            filePath = filePath.concat(args[0]);
+        }
+        List<Equality> equalities = FileParser.parseFile(filePath);
+        UnitGraph unitGraph = UnitGraph.fromEqualityList(equalities);
+        List<Equality> notFilledEqualities = equalities.stream()
+                                                        .filter(equality -> equality.getRightValue() == null)
+                                                        .collect(Collectors.toList());
+        notFilledEqualities.forEach(unitGraph::calculateEquality);
+        writeResultFile(notFilledEqualities);
+    }
+
+    private static void writeResultFile(List<Equality> equalities) {
+        File out = new File(getCurrentPath() + "result.txt");
+        try {
+            FileWriter fileWriter = new FileWriter(out);
+            for (Equality equality: equalities) {
+                if (equality.getRightValue() == null) {
+                    fileWriter.write("Конвертация невозможна");
+                } else {
+                    fileWriter.write(equality.toString());
+                }
+                fileWriter.write(System.lineSeparator());
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getCurrentPath() {
+        String path = Converter.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        for (int i = 1; i <= 2; i++) {
+            path = path.substring(0, path.lastIndexOf(File.separator));
+        }
+        path = path.concat(File.separator);
+        return path;
+    }
+
+}
